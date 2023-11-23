20188d2c917b46406745d7590bb9f9715c0562c8
Diego <diego.4braham@gmail.com>
2023-11-22 19:37:55 -0600

    22-11-23 DAAT

--- src/app2.py
+++ src/app2.py
@@ -8,6 +8,20 @@ app=Flask(__name__)
 
 con=MySQL(app)
 
+def leer_alumno_bd(mat):
+    try:
+            cursor=con.connection.cursor()
+            sql="select * from alumnos where matricula= {0}".format(mat)
+            cursor.execute(sql)
+            datos=cursor.fetchone()
+            if datos!=None:
+                alumno={'matricula': datos[0], 'nombre':datos[1], 'apaterno':datos[2],
+                    'amaterno':datos[3], 'correo':datos[4]}
+                return alumno
+            else:
+                return None
+    except Exception as ex:
+                raise ex
 
 @app.route('/alumnos', methods=['GET'])
 def list_alumnos():
@@ -29,19 +43,18 @@ def list_alumnos():
         return jsonify({'mensaje': '{}'.format(ex)})
 
 
-@app.route('/alumnos/<int:mat>', methods=['GET'])
+@app.route('/alumnos/<mat>', methods=['GET'])
 def leer_alumno(mat):
     try:
-            cursor=con.connection.cursor()
-            sql="select * from alumnos where matricula= '{0}'".format(mat)
-            cursor.execute(sql)
-            datos=cursor.fetchone()
-            if datos!=None:
-                alum=alum={'matricula': fila[0], 'nombre':fila[1], 'apaterno':fila[2],
-                'amaterno':fila[3], 'correo':fila[4]}
-                return jsonify({'Alumnos': alum, ' mensaje':'el alumno es'})
-        except Exception as ex:
-            return jsonify({'mensaje': '{}'.format(ex)})
+            alumno=leer_alumno_bd(mat)
+            if alumno != None:
+                return jsonify({'Alumnos':alumno, 'mensaje':'el alumno encontrado', 'exito':True})
+            else:
+                return jsonify({"mensaje":' Alumno no encontrado', 'exito':False})
+
+    except Exception as ex:
+                return jsonify({'mensaje': '{}'.format(ex)})
+
 def pagina_no_encontrada(error):
     return '<h1> Pagina no encontrada unu </h1>', 404
 
